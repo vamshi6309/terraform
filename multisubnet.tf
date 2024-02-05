f7e0a0a6d6a656de163b052eda24c40c227ce92f
vamshi6309 <vamshivenkatreddy5@gmail.com>
2024-02-05 17:42:39 +0530

    8th commit

---
+++ multisubnet.tf
@@ -0,0 +1,21 @@
+resource "aws_db_subnet_group" "example1" {
+  name       =  "my-db-subnet-group"
+  description = "My DB subnet group"
+  subnet_ids = [
+    aws_subnet.publicsubnet1.id,
+    aws_subnet.publicsubnet2.id,
+
+  ]
+}
+
+resource "aws_db_subnet_group" "example2" {
+  name       =  "my-db-subnet-group1"
+  description = "My DB subnet group"
+  subnet_ids = [
+    aws_subnet.privatesubnet1.id,
+    aws_subnet.privatesubnet2.id,
+    
+
+  ]
+}
+
