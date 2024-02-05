96bfaf42235984e6f5af13a4a60edd0815c4a968
vamshi6309 <143407489+vamshi6309@users.noreply.github.com>
2024-02-05 18:19:33 +0530

    Update database.tf

--- database.tf
+++ database.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "default1new" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.sg.id]
-  db_subnet_group_name = aws_db_subnet_group.example1new.name
+  db_subnet_group_name = aws_db_subnet_group.example1new12.name
 
    multi_az             = true
   
@@ -26,7 +26,7 @@ resource "aws_db_instance" "default2new" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.sg.id]
-  db_subnet_group_name = aws_db_subnet_group.example2new.name
+  db_subnet_group_name = aws_db_subnet_group.example2new12.name
 
   
   
